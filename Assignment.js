3b5d60ca6d58081bd013eda32a218168128f2540
josephomohwovo <josephomohwovo@gmail.com>
2023-01-24 21:43:33 +0100

    Second Assignment solution

---
+++ Assignment.js
@@ -0,0 +1,13 @@
+// create a function that returns my name.
+// Takes my firstname and lastname as parameters
+// also age para
+
+// sltn
+// My ezekeil Adetoro I am 23 years old
+function fullName(firstName, lastName){
+    return firstName + " " + lastName;
+  }
+  var fullname = fullName("Ezekial", "Adetoro");
+  
+  console.log(fullname)
+  console.log("My name is ezekeil Adetoro I am 23 years old")
